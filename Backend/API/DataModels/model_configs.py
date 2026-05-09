10bd944171c0268e399a4c07df58b12ee9d40336
AquaWoid <luwa00@hotmail.com>
2026-05-09 12:48:51 +0200

    Removed leftover files

--- Backend/API/DataModels/model_configs.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class topic_model_settings(BaseModel):
-    n_neighbours: int
-    n_components: int
-    min_cluster_size: int
-    min_samples: int    
-
--- Backend/API/LLM/codebook_creation.py
+++ Backend/API/LLM/codebook_creation.py
@@ -62,68 +62,36 @@ Output ONLY the JSON object. Nothing else.
 If the output is not in the input language translate it.
 """
 
-user_prompt = """
 
-So, fast. Ja, also im Endeffekt, also ich bin Masterstudent in Graz an der TH-Fakultät und jetzt bald am fertig werden und wir haben das Projektseminar, das ist immer ein Seminar, in dem wir sozusagen ein Forschungsprojekt machen müssen, um
-hilflich sein soll, Daten zu verarbeiten, leichter zu verarbeiten, zugänglich zu machen und da bin ich jetzt eben derzeit noch in der Use-Case-Analyse sozusagen, das heißt, ich mache jetzt eben mit dir und einem und zwei anderen nur Interviews dazu, inwiefern man so ein Tool circa für die jeweiligen Projekte nutzen könnte.
-Das wäre jetzt bei dir eben auf das ManMax bezogen, da war jetzt nicht sicher, das war eben der Georg auch nicht sicher, inwiefern du da mit den Daten direkt arbeitest, also mit den Hintergrunddaten sozusagen.
-Wie viel quasi weißt du vom Projekt, also was kann ich quasi voraussetzen an Wissen?
-Also ich habe diesen JSON-Datensatz von Herrn Heiden bekommen, den habe ich mir ein bisschen durchgeschaut und ich habe mir sozusagen die Website vom Projekt ein bisschen angeschaut, also das ist meine Basis.
-Okay, verstehe. Also ich war jemand, der geholfen hat, diese Daten auch zu säubern, also er hat so von einer sehr veralteten Software, von einer älteren Dame bekommen,
-diesen Grundstock, das Schuhregister, ich weiß nicht, ob da das versagt, und das war halt so Excel-Tabelle, weil sie so absolut, ihr Mann hat irgendeinen Flashplayer-Ding gemacht, damit sie leichter Buchregister schreiben kann damit.
-Also so ist das Ganze entstanden. Und deswegen haben wir rübergehen müssen, weil das Programm, das der Herr Heiden eben verwendet hat,
-er hat zum Beispiel Leute wie den Kaiser Maximilian als Ort getaggt, automatisch, weil er es nicht gescheit gekannt hat.
-Deswegen war unsere erste Aufgabe, war das mal zu säubern und uns überhaupt die Frage zu stellen, bei manchen Sachen, also beim Kaiser war es natürlich leicht zu sagen,
-okay, gut, das ist eine Person und kein Ort. Aber zum Beispiel, wenn es darum ging, was machst du mit einer Stadtgemeinde?
-Weil das ist einerseits ein Ort und andererseits eine Personengruppe und dann eigentlich wieder auch eine Organisation.
-Und das war für uns am Anfang total schwer, auch das, wir haben sehr viel gestritten untereinander, sagen wir es mal so,
-weil manche Sachen waren halt nicht so leicht. Und jetzt arbeite ich ein bisschen weniger damit, weil ich schreibe Registen und ich tagge nur die Personen, Orte, Organisationen, die da vorkommen.
-Und das ist quasi momentan mein Beitrag zur Datenbank. Also momentan ist recht wenig. Am Anfang war ich sehr viel beteiligt damit, ja.
-Okay. Und mir ist jetzt auch noch die Frage aufgekommen, eben, habt ihr dieses Factor jetzt Chasen bekommen, was spielt das im Projekt für eine Rolle sozusagen?
-Ist das sozusagen der Grunddatensatz von dem Projekt?
-Ja, das ist quasi, soll auch das große Ergebnis dann sein, dass wir quasi die tollste, beste Datenbank in global haben zum Maximilian.
-Das ist der Anspruch, den wir in unseren Projektleiter gesetzt haben, ja.
-Okay, und wird da jetzt schon irgendwie interagiert mit den Daten derzeit? Oder ist das nur im Aufbau sozusagen?
-Beides. Also das Problem ist, wir haben halt diesen Grundstock, den man gekriegt hat, eben dieses Schuhregister.
-Ja.
-Und nebenbei soll man halt parallel auch natürlich in die Datenbank reinarbeiten.
-Aber manche Kollegen von uns haben halt jetzt schon sehr viel reingearbeitet, zum Beispiel für ihre speziellen Interessen.
-Ja.
-Und die haben sich das auch schon rausgeholt von der, ich kann nur ihren Vornamen, von der Marcella, ich weiß nicht, ob du die schon mal gesehen hast.
-Sie unterrichtet jetzt auch in Graz.
-Die macht viel Netzwerkanalyse.
-Mhm.
-Und die hat halt geholfen.
+def create_codebook(prompt: str):
 
+  headers = {
+      'Content-Type': 'application/json',
+  }
 
-"""
+  json_data = {
+      'model': 'Qwen/Qwen3-14B-AWQ',
+      'messages': [
+          {
+              'role': 'system',
+              'content': f'{system_prompt}',
+          },
+          {
+              'role': 'user',
+              'content': f'{prompt}',
+          },
+      ],
+  }
 
-headers = {
-    'Content-Type': 'application/json',
-}
+  response = requests.post('http://localhost:8000/v1/chat/completions', headers=headers, json=json_data)
 
-json_data = {
-    'model': 'Qwen/Qwen3-14B-AWQ',
-    'messages': [
-        {
-            'role': 'system',
-            'content': f'{system_prompt}',
-        },
-        {
-            'role': 'user',
-            'content': f'{user_prompt}',
-        },
-    ],
-}
+  print(response.text)
 
-response = requests.post('http://localhost:8000/v1/chat/completions', headers=headers, json=json_data)
+  data = response.json()
+  content = data["choices"][0]["message"]["content"]
 
-print(response.text)
+  content = re.sub(r"<think>.*?</think>", "", content, flags=re.DOTALL).strip()
+  print(content)
 
-data = response.json()
-content = data["choices"][0]["message"]["content"]
+  return content
 
-content = re.sub(r"<think>.*?</think>", "", content, flags=re.DOTALL).strip()
-print(content)
-
-#data = json.loads(response)
